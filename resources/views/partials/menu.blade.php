f55c456419ef3b6f35649d672fc1aca0337067c5
jonas <jonas.skott.nielsen@live.dk>
2020-11-27 17:34:38 +0100

    added menu and partials views

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,7 @@
+<div class="topnav">
+  <a class="active" href="#home">Forside</a>
+  <a href="#news">Aktier</a>
+  <a href="#contact">Crowdlending</a>
+  <a href="#about">FIRE</a>
+  <a href="#about">Login</a>
+</div>
