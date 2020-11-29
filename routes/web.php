88499946550e5c154ac2a0c937b26e9eedc4cbf6
jonas nielsen <jonas18@student.sdu.dk>
2020-11-29 19:10:30 +0100

    added register and db

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/register', [RegisterController::class, 'index']);
 
 Route::get('/pages', function () {
     return view('pages.welcome');
